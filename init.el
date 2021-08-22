7ca5ff154e804656c8200fca494fe74695db406d
Kevin Pavao <kpavao84@gmail.com>
2021-08-21 22:36:58 -0500

    update all packages, use example setup for modus-themes

--- init.el
+++ init.el
@@ -188,39 +188,15 @@
   ;; Monospaced typeface
   (set-face-attribute 'fixed-pitch nil :family "DejaVu Sans Mono" :height 1.0))
 
-(use-package modus-operandi-theme
+(use-package modus-themes
   :straight t
-  :custom
-  (modus-operandi-theme-bold-constructs t)
-  (modus-operandi-theme-slanted-constructs t)
-  (modus-operandi-theme-completions 'opinionated)
-  ;; org specific settings
-  (modus-operandi-theme-org-blocks 'grayscale)
-  (modus-operandi-theme-headings '((t . rainbow)))
-  (modus-operandi-theme-scale-headings t)
+  :init
+  (setq modus-themes-italic-constructs t
+        modus-themes-bold-constructs nil
+        modus-themes-region '(bg-only no-extend))
+  (modus-themes-load-themes)
   :config
-  (load-theme 'modus-operandi))
-
-(use-package modus-vivendi-theme
-  :straight t
-  :custom
-  (modus-vivendi-theme-bold-constructs t)
-  (modus-vivendi-theme-slanted-constructs t)
-  (modus-vivendi-theme-completions 'opinionated)
-  ;; org specific settings
-  (modus-vivendi-theme-org-blocks 'grayscale)
-  (modus-vivendi-theme-headings '((t . rainbow)))
-  (modus-vivendi-theme-scale-headings t))
-
-(defun modus-themes-toggle ()
-    "Toggle between `modus-operandi' and `modus-vivendi' themes."
-    (interactive)
-    (if (eq (car custom-enabled-themes) 'modus-operandi)
-        (progn
-          (disable-theme 'modus-operandi)
-          (load-theme 'modus-vivendi t))
-      (disable-theme 'modus-vivendi)
-      (load-theme 'modus-operandi t)))
+  (modus-themes-load-operandi))
 
 (provide 'init)
 ;;; init.el ends here
