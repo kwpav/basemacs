734e4eedc682fc69725b30685124efd806fb108b
Kevin Pavao <kpavao84@gmail.com>
2021-10-10 13:20:20 -0500

    use gfhook for vterm, wording changes

--- lisp/kwp-vterm.el
+++ lisp/kwp-vterm.el
@@ -12,13 +12,9 @@
    "tt" 'multi-vterm
    "tn" 'multi-vterm-next
    "tp" 'multi-vterm-prev)
+  :gfhook #'evil-insert-state
   :config
-  (add-hook 'vterm-mode-hook
-            (lambda ()
-              ;; (setq-local evil-insert-state-cursor 'box)
-              (evil-insert-state)))
   (define-key vterm-mode-map [return]                      #'vterm-send-return)
-
   (setq vterm-keymap-exceptions nil)
   (evil-define-key 'insert vterm-mode-map (kbd "C-e")      #'vterm--self-insert)
   (evil-define-key 'insert vterm-mode-map (kbd "C-f")      #'vterm--self-insert)
